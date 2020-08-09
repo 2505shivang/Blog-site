8bee9956c333f7439b9e143f12eff6f201eedb28
imemya19 <45592644+imemya19@users.noreply.github.com>
2020-08-09 22:31:14 +0530

    Final and demo video

--- middleware/index.js
+++ middleware/index.js
@@ -5,48 +5,48 @@ var Comment = require("../models/comment");
 var middlewareObj = {};
 
 middlewareObj.checkBlogOwnership = function (req, res, next) {
-    if (req.isAuthenticated()) {
-        Blog.findById(req.params.id, function (err, foundBlog) {
-            if (err) {
-                res.redirect("/blogs/");
-            } else {
-                // does user own the blog?
-                if (foundBlog.user_id.equals(req.user._id)) {
-                    next();
-                } else {
-                    res.redirect("/blogs/"+req.params.id);
-                }
-            }
-        });
-    } else {
-        res.redirect("/blogs");
-    }
-}
+  if (req.isAuthenticated()) {
+    Blog.findById(req.params.id, function (err, foundBlog) {
+      if (err) {
+        res.redirect("/blogs/");
+      } else {
+        // does user own the blog?
+        if (foundBlog.user_id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("/blogs/" + req.params.id);
+        }
+      }
+    });
+  } else {
+    res.redirect("/blogs");
+  }
+};
 
 middlewareObj.checkCommentOwnership = function (req, res, next) {
-    if (req.isAuthenticated()) {
-        Comment.findById(req.params.comment_id, function (err, foundComment) {
-            if (err) {
-                res.redirect("/blogs");
-            } else {
-                // does user own the comment?
-                if (foundComment.user_id.equals(req.user._id)){
-                    next();
-                } else {
-                    res.redirect("/blogs");
-                }
-            }
-        });
-    } else {
-        res.redirect("back");
-    }
-}
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id, function (err, foundComment) {
+      if (err) {
+        res.redirect("/blogs");
+      } else {
+        // does user own the comment?
+        if (foundComment.user_id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("/blogs/" + req.params.id);
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
+};
 
 middlewareObj.isLoggedIn = function (req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect("/login");
-}
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+};
 
 module.exports = middlewareObj;
